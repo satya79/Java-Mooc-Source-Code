b0c55f14332ebb69181e9b445730cc147fdefc2c
Satya <satya@gmail.com>
2024-05-14 12:59:11 -0700

    minor edit

---
+++ OnceUponATime.java
@@ -0,0 +1,12 @@
+
+public class OnceUponATime {
+
+    // code
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+        
+    }
+}
