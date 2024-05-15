f06a93dcd3eaa25685c32760cbe8807f14e733d0
Satya <satya@gmail.com>
2024-05-15 12:00:16 -0700

    p6

---
+++ Conversation.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Conversation{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Greetings! How are you doing?");
+        String first_string = scanner.nextLine();
+        //System.out.println(first_string);
+        System.out.println("Oh, how interesting. Tell me more!");
+        String second_string = scanner.nextLine();
+       // System.out.println(second_string);
+        System.out.println("Thanks for sharing!");
+        
+        
+        
+        
+    
+    
+    }
+
+
+}
