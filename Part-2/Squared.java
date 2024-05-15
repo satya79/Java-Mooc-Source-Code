ebaa9759cc5d53a8bb484ecd7ad1c5d5da92c4d6
Satya <satyasahu7913@gmail.com>
2024-05-15 13:14:49 -0700

    p13

---
+++ Part-2/Squared.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.valueOf(scanner.nextLine());
+        int num2 = num1*num1;
+        System.out.println(num2);
+        
+                
+
+    }
+}
