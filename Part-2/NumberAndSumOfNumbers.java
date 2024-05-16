c1955ddcf43b0d5bdd9e6edea05b579fdc287754
Satya <satyasahu7913@gmail.com>
2024-05-15 21:41:32 -0700

    new

---
+++ Part-2/NumberAndSumOfNumbers.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class NumberAndSumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        int count = 0;
+        while(true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == 0) {
+                break;
+            } else {
+                total += number;
+                count++;
+            }
+        }
+        System.out.println("Number of numbers: " + count);
+        System.out.println("Sum of the numbers: " + total);
+        
+
+    }
+}
