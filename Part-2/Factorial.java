c11bf9ae03497b2500e8d5c99f4d1140f8188b70
Satya <satyasahu7913@gmail.com>
2024-05-16 10:06:10 -0700

    new

---
+++ Part-2/Factorial.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Write your program here
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(scanner.nextLine());
+        int total = 1;
+        for (int i = 1; i <= number; i++) {
+            total *= i;
+        }
+        System.out.println("Factorial: " + total);
+        
+
+    }
+}
