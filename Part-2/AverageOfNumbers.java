05176fad6080298d0ce86bb598b44b6b93d4f61f
Satya <satyasahu7913@gmail.com>
2024-05-15 21:41:54 -0700

    p14

---
+++ Part-2/AverageOfNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Write your program here
+        int total = 0;
+        int count = 0;
+        while(true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == 0) {
+                break;
+            } else {
+                total += number;
+                count++;
+            }
+        }
+        System.out.println("Average of the numbers: " + total / (double)count);
+            
+    }        
+
+}
+
+
