439b4ab0ddb16dce5b1bdc3928a208768b915f69
Satya <satyasahu7913@gmail.com>
2024-05-16 10:04:19 -0700

    new

---
+++ Part-2/CountingToHundred.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class CountingToHundred {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = n; i<=100; i++ ){
+            System.out.println(i);
+        }
+
+    }
+}
